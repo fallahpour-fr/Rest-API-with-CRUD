e1d993758588dfaaa67701cc644f5fe969509491
fallahpour-fr <farzanehhh.fl@gmail.com>
2024-08-17 00:07:43 -0700

    add classes

--- models/users.model.js
+++ models/users.model.js
@@ -1,26 +1,49 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = new Sequelize('mysql://root:my-secret-pw@localhost:3306/crud') // Example for postgres
+// const { Sequelize, DataTypes } = require('sequelize');
+// const sequelize = new Sequelize('mysql://root:my-secret-pw@localhost:3306/crud') // Example for postgres
 
-const User = sequelize.define(
-    'User',
-    {
-        id: {
-            allowNull: false,
-            autoIncrement: true,
-            primaryKey: true,
-            type: DataTypes.INTEGER,
-        },
-        name: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
+// const User = sequelize.define(
+//     'User',
+//     {
+//         id: {
+//             allowNull: false,
+//             autoIncrement: true,
+//             primaryKey: true,
+//             type: DataTypes.INTEGER,
+//         },
+//         name: {
+//             type: Sequelize.STRING,
+//             allowNull: false,
+//         },
+//     },
+//     {
+//         // Other model options go here
+//         timestamps:false,
+//         // tableName:"Users"
+//     },
+// );
+
+// module.exports=User;
+
+const { Sequelize, DataTypes, Model } = require('sequelize');
+const sequelize = new Sequelize('mysql://root:my-secret-pw@localhost:3306/crud'); 
+
+class User extends Model {}
+
+User.init({
+    id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
     },
-    {
-        // Other model options go here
-        timestamps:false,
-        // tableName:"Users"
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
     },
-);
+}, {
+    sequelize, 
+    modelName: 'User', 
+    timestamps: false, 
+});
 
-module.exports=User;
-
+module.exports = User;
