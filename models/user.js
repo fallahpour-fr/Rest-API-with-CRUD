d511a58d55ee67598f35ba4be127ea1297f72bb5
fallahpour-fr <farzanehhh.fl@gmail.com>
2024-08-20 03:07:59 -0700

    add users migrations and users model

--- models/user.js
+++ models/user.js
@@ -22,6 +22,14 @@ User.init({
         type: DataTypes.STRING,
         allowNull: false,
     },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
 },
     {
         sequelize,
