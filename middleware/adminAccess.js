a23be46e82d300ff7076de24fb64a06621c36cf0
fallahpour-fr <farzanehhh.fl@gmail.com>
2024-08-28 00:38:35 -0700

    fix admin access

--- middleware/adminAccess.js
+++ middleware/adminAccess.js
@@ -2,29 +2,30 @@ const { User, Role, Post, Permission, UserRole } = require('../models');
 
 module.exports = async (req, res, next) => {
     console.log("body name", req.body.name);
-    console.log("user id", req.user.id)
+    console.log("user id", req.user)
     try {
         const currentId = req.user.id;
+
         Role.findAll({
             include: [{
-                model: User,
-                where: { id: currentId },
-                attributes: []
+                model: User, // The join model (user_role)
+                where: { id: currentId }, // Condition to filter by userId
+                attributes: [] // Exclude attributes from the join table
             }],
-            attributes: ['name']
+            attributes: ['name'] // Only select the 'name' field from the Roles table
         })
             .then(roles => {
-                console.log(roles)
-                if (roles.length > 0) {
-                    const roleNames = roles.map(role => role.name);
-                    console.log(roleNames); // Output: ['Admin', 'Editor']
-                } else {
-                    console.log('No role found for this user.');
+                const roleNames = roles.map(role => role.name); // Extract the role names
+                if(roleNames[0]=='Admin'){
+                    next()
+                }else{
+                    console.log('Not Access')
                 }
             })
             .catch(error => {
-                console.error('Error:', error);
+                console.error('Error fetching roles:', error);
             });
+
 
     } catch (error) {
         console.log("error", error);
--- middleware/auth.js
+++ middleware/auth.js
@@ -3,38 +3,14 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     const token = req.header('Authorization')?.replace('Bearer ', '');
-    console.log("token", token)
+
     if (!token) {
         return res.status(401).json({ message: 'Access denied. No token provided.' });
     }
 
-    // const decoded = jwt.verify(token, SECRET_KEY);
-    // console.log("decoded",decoded)
-
     try {
         const decoded = jwt.verify(token, process.env.SECRET_KEY);
-        console.log("decoded", decoded)
         req.user = decoded;
-
-        // بازیابی کاربر از دیتابیس
-        // const user = await User.findByPk(req.user.id, {
-        //     include: [Role, Permission] // بارگذاری نقش‌ها و مجوزها
-        // });
-
-        // if (!user) {
-        //     return res.status(401).json({ message: 'User not found.' });
-        // }
-        // // بررسی نقش کاربر
-        // if (requiredRole && !user.Roles.some(role => role.name === requiredRole)) {
-        //     return res.status(403).json({ message: 'Access denied. You do not have the required role.' });
-        // }
-
-        // // بررسی مجوز کاربر
-        // if (requiredPermission && !user.Permissions.some(permission => permission.name === requiredPermission)) {
-        //     return res.status(403).json({ message: 'Access denied. You do not have the required permission.' });
-        // }
-
-
         next();
     } catch (error) {
         res.status(400).json({ message: 'Invalid token.' });
